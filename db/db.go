2dd71c0d7650a66dac78a20d608b01ce07addb65
Catalin Soare <stevensuncatalin369@gmail.com>
2024-11-08 17:21:41 +0200

    Setup the database connection.

---
+++ db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"api/env"
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Ctx = context.Background()
+
+var Client *mongo.Client
+
+func InitDB() (err error) {
+	Client, err = mongo.Connect(
+		Ctx,
+		options.Client().ApplyURI(env.MONGO_URI),
+	)
+
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Connected to MongoDB")
+	return nil
+}
+
+func GetCollection(collectionName string) *mongo.Collection {
+	return Client.Database("dev").Collection(collectionName)
+}
