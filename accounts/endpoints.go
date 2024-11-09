7eaed0c6b90623a03fae0121d4ab7158cfe162bd
Catalin Soare <stevensuncatalin369@gmail.com>
2024-11-09 12:31:36 +0200

    added auth for account and dispatcher. now onto working on teams and ambulances models.

--- accounts/endpoints.go
+++ accounts/endpoints.go
@@ -1,11 +1,57 @@
 package accounts
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"api/models"
+	"api/utils"
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v3"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Endpoints(app *fiber.App) {
 	acc := app.Group("/accounts")
 
 	acc.Get("/ping", func(c fiber.Ctx) error {
 		return c.SendString("PONG\n")
+	})
+
+	acc.Get("/whoami", func(c fiber.Ctx) error {
+		var account models.Account
+		utils.GetLocals(c, "account", &account)
+
+		return c.JSON(account)
+	}, models.AccountMiddleware)
+
+	acc.Post("/login", func(c fiber.Ctx) error {
+		var body struct {
+			Phone    string `json:"phone"`
+			Passcode string `json:"passcode"`
+		}
+		json.Unmarshal(c.Body(), &body)
+
+		var account models.Account
+		err := account.GetByPhone(body.Phone)
+		if err != nil {
+			return utils.Error(c, err)
+		}
+
+		err = bcrypt.CompareHashAndPassword(
+			[]byte(account.Passcode),
+			[]byte(body.Passcode),
+		)
+		if err != nil {
+			return utils.Error(c, err)
+		}
+
+		token := account.GenToken()
+
+		return c.JSON(
+			bson.M{
+				"token":   token,
+				"account": account,
+			},
+		)
 	})
 }
--- dispatchers/endpoints.go
+++ dispatchers/endpoints.go
@@ -1,6 +1,12 @@
 package dispatchers
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"api/models"
+	"api/utils"
+
+	"github.com/gofiber/fiber/v3"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 func Endpoints(app *fiber.App) {
 	dis := app.Group("/dispatchers")
@@ -8,5 +14,30 @@ func Endpoints(app *fiber.App) {
 	dis.Get("/ping", func(c fiber.Ctx) error {
 		return c.SendString("PONG\n")
 	})
+
+	dis.Get("/whoami", func(c fiber.Ctx) error {
+		var dis models.Dispatcher
+		utils.GetLocals(c, "dispatcher", &dis)
+
+		return c.JSON(dis)
+	})
+
+	dis.Post("/auth", func(c fiber.Ctx) error {
+		var account models.Account
+		utils.GetLocals(c, "account", &account)
+
+		var dispatcher models.Dispatcher
+		err := dispatcher.GetByAccount(account.ID)
+		if err != nil {
+			return utils.Error(c, err)
+		}
+
+		token := dispatcher.GenToken(account)
+		return c.JSON(bson.M{
+			"dispatcher": dispatcher,
+			"account":    account,
+			"token":      token,
+		})
+	}, models.AccountMiddleware)
 
 }
